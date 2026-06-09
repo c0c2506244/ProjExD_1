482561c275983c36cf5508a4314f86da0e5acb27
C0C25062 <c0c2506244@edu.teu.ac.jp>
2026-06-09 15:57:28 +0900

    練習7と8と9

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg2_img = pg.transform.flip(bg_img,True,False) #練習8：bg_imgを反転
     kk_img = pg.image.load("fig/3.png") # 練習３：こうかとん入れる
     kk_img = pg.transform.flip(kk_img,True,False) #練習3：左右反転
     tmr = 0
-    x=0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img, [300, 200]) #練習４：
+        x = tmr % 3200 #練習5 & 練習9
+        screen.blit(bg_img, [-x, 0])#練習５
+        screen.blit(bg2_img,[-x+1600,0]) #練習7：連続させる
+        screen.blit(bg_img,[-x+3200,0]) #練習9
+        screen.blit(kk_img, [300, 200]) #練習４
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習6
